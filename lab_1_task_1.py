ec30ad82f4aece712719fe14ccf76946de5ad212
inf3-11 <inf3-11@maoulic23.rt>
2023-09-26 14:42:21 +0200

    lab1

---
+++ lab_1_task_1.py
@@ -0,0 +1,5 @@
+print("hello,World")
+print('Маша+Петя=Любовь')
+print('x=3+4')
+x=3+4
+print(f'{x}')
---
+++ lab_1_task_2.py
@@ -0,0 +1,12 @@
+
+a='Hello, World'
+print(type(a))
+
+b=3
+print(type(b))
+
+c=3/4
+print(type(c))
+
+d=[1, 2, 5, 10, 100]
+print(type(d))
