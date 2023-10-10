64d0a636ed6faeb02595c967af88cc4f48287480
ppolkh <polkh.pauline@yandex.ru>
2023-10-10 14:42:17 +0200

    ojh
    q

--- lab_1_task_1.py
+++ lab_1_task_1.py
@@ -2,4 +2,4 @@ print("hello,World")
 print('Маша+Петя=Любовь')
 print('x=3+4')
 x=3+4
-print(f'{x}')
+print(f'ч = {x}')
---
+++ lab_1_task_4.py
@@ -0,0 +1,14 @@
+a=[1, 5, 'Good', 'Bad']
+b=[9, 'Blue', 'Red', 11]
+c=a[1]
+d=b[3]
+e=(c+d)
+print(f'{e}')
+f=a[2]
+g=b[2]
+h=(f+g)
+print(f'{h}')
+i=a[0]
+j=b[0]
+k=i+j
+print(f'{k}')
